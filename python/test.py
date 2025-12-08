350692553301347f20cca285baf78a21b8ee8110
mtfmtf <m.torres@student.uni-kassel.de>
2025-12-08 14:14:57 +0100

    new updates

--- python/test.py
+++ python/test.py
@@ -2,7 +2,15 @@ import sys
 import os
 from datetime import datetime
 
-#DIRECTORY=f'..{os.sep}docs{os.sep}'
+'''
+file tree in docker by now
+app
+|-user -> csv with user data
+|-docs -> for md-files
+test.py
+
+'''
+
 DIRECTORY=f'docs{os.sep}'
 HEADER='---\n---\n{% include nav.html %}'
 
@@ -18,7 +26,15 @@ def restore_print():
   sys.stdout=sys.__stdout__
 
 if __name__=="__main__":
-  print(f'TEST on {datetime.now().strftime("%Y%m%d-%H%M%S")}')
+  # print(f'TEST on {datetime.now().strftime("%Y%m%d-%H%M%S")}')
   print_to_file('test.md')
   print(f'last run: {datetime.now().strftime("%Y%m%d-%H%M%S")}\n')
-  
+  restore_print()
+  for file in os.listdir("user"):
+      if file.endswith("csv"):
+          ff = f'user{os.sep}{file}'
+          with open(f'{ff}', 'r') as f:
+              a = f.readline()
+              while a:
+                  print(a)
+                  a = f.readline()
