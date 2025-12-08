936e91af12f37e8213eaa11a95d2a939794fdf44
mtfmtf <149099960+mtfmtf@users.noreply.github.com>
2025-12-08 16:59:16 +0100

    Actualizar update_nav.py

--- python/update_nav.py
+++ python/update_nav.py
@@ -7,4 +7,4 @@ def update_nav(time):
     with open(f'{FILE}', 'w') as new_nav:
         new_nav.write('<nav style="margin-bottom: 20px;"> <a href="home.html">home</a> |  <a href="seite2.html">seite '
                       '2</a> | <a href="seite3.html">seite 3</a> | <a href="test.html">test file</a> </nav>')
-        new_nav.write(f'<font color="#FF0000"><b>last update: {time}</b></font><font color="#CDCD00">')
+        new_nav.write(f'<font color="#FF0000"><b>last update: {time}</b></font><font color="#CDCD00">\n')
