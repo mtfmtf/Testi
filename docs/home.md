1634ec71c7a8d4181392ec3a47f0b0da087a025e
ElenaSotoSobrino <elena.soto@web.de>
2025-12-09 18:27:35 +0100

    add: logout function + button
    add logout.html as button in home.md and logic in main.js
    
    cloned index.html to start.html because of compiling error -> two index.html (index.md compiled to home.html)

--- docs/home.md
+++ docs/home.md
@@ -3,14 +3,14 @@
 
 {% include nav.html %}
 
-
-
 # Hallo Welt!
+
 Dies ist meine GitHub-Pages Seite in **Markdown**.
 
 [Zu unseren Mockups 👀](allgemein.html)
 
 [Zu Testdatei 👀](test.html)
 
-
 [Wikipedia](https://de.wikipedia.org/wiki/Wikipedia:Hauptseite) 
+
+{% include logout.html %}
--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,32 @@
-document.getElementById('login-form').addEventListener('submit', function(e){
-    e.preventDefault();
-    const user = document.getElementById('username').value;
-    const pass = document.getElementById('password').value;
-
-    // Einfacher Check
-    if(user === "admin" && pass === "1234"){
-        window.location.href = "home.html";
-    } else {
-        alert("Benutzername oder Passwort falsch!");
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById('login-form');
+    if (loginForm) {
+        loginForm.addEventListener('submit', function(e){
+            e.preventDefault();
+            const user = document.getElementById('username').value;
+            const pass = document.getElementById('password').value;
+            if(user === "admin" && pass === "Projekt2025"){
+                window.location.href = "home.html";
+            } else {
+                alert("Benutzername oder Passwort falsch!");
+            }
+        });
     }
+
+    const logoutForm = document.getElementById('logout-form');
+    if (logoutForm) {
+        logoutForm.addEventListener('submit', function(e){
+            e.preventDefault();
+            window.location.href = "start.html"; 
+        });
+    }
+
+    const togglePassword = document.getElementById('toggle-password');
+    const password = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function() {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        this.textContent = type === 'password' ? '👁️' : '🙈';
+    });
 });
